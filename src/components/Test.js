266aa3a42a2bb7556a717633d57d5653b5f7b88d
Faizal Zakaria <fai@code3.io>
2017-12-08 14:01:24 +0100

    Init commit

---
+++ src/components/Test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class Test extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Test Component</h1>
+      </div>)
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Test from './components/Test.js'
+
+ReactDOM.render(<Test />, document.getElementById('root'))
